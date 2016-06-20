d3380fa79a852bff042c4c1a3cd524060dc283c2
Tim Vaughan <tgvaughan@gmail.com>
2016-06-20 21:38:08 +1200

    Clean up BEAUti inerface a bit.

--- src/beast/math/distributions/ParameterListsPrior.java
+++ src/beast/math/distributions/ParameterListsPrior.java
@@ -1,41 +1,50 @@
 package beast.math.distributions;
 
 import beast.core.Description;
+import beast.core.Distribution;
 import beast.core.Input;
+import beast.core.State;
 import beast.core.parameter.ParameterList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author Chieh-Hsi Wu
  */
 @Description("This class is a wrapper that provides prior to parameter list.")
-public class ParameterListsPrior extends Prior{
+public class ParameterListsPrior extends Distribution {
 
-    public Input<List<ParameterList>> xListsInput = new Input<List<ParameterList>>(
+    final public Input<ParametricDistribution> distInput = new Input<>(
+            "distr",
+            "distribution used to calculate prior, e.g. normal, beta, gamma.",
+            Input.Validate.REQUIRED);
+
+    public Input<List<ParameterList>> xListsInput = new Input<>(
             "xList",
             "points at which the density is calculated",
             new ArrayList<ParameterList>(),
             Input.Validate.REQUIRED
     );
 
-    public Input<Boolean> applyToListInput = new Input<Boolean>(
+    public Input<Boolean> applyToListInput = new Input<>(
             "applyToList",
             "Whether the prior is applied to the entire list",
             Input.Validate.REQUIRED
     );
 
-    public ParameterListsPrior(){
-        m_x.setRule(Input.Validate.OPTIONAL);
-    }
+    protected ParametricDistribution dist;
+
+    public ParameterListsPrior() { }
 
     boolean applyToList;
     public void initAndValidate(){
 
         applyToList = applyToListInput.get();
-        super.initAndValidate();
 
+        dist = distInput.get();
+        calculateLogP();
     }
 
     @Override
@@ -47,6 +56,16 @@ public class ParameterListsPrior extends Prior{
 	}
 
     @Override
+    public List<String> getArguments() { return null; }
+
+    @Override
+    public List<String> getConditions() { return null; }
+
+    @Override
+    public void sample(State state, Random random) {
+
+    }
+
     public String getParameterName() {
     	String name = "";
     	for (ParameterList list : xListsInput.get()) {
